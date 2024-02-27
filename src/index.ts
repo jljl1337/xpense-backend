bf38659bb12ce929f094c65450206a9997271f84
Jack Leung <lckjack123@gmail.com>
2024-02-27 22:03:58 +0800

    Read server port from .env

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,13 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import router from './router';
 
-const app = express();
+const PORT = Number(process.env.PORT);
 
+const app = express();
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+server.listen(PORT, () => {
+  console.log('Server is running on port: %d', PORT);
 });
 
 app.use(cors({credentials: true}));
