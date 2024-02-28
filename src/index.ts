ee9072f9232fca48e88dc186af6916ddedc272ae
Jack Leung <lckjack123@gmail.com>
2024-02-28 11:48:34 +0800

    Update server to use HTTPS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
@@ -9,18 +10,25 @@ import mongoose from 'mongoose';
 import router from './router';
 
 const PORT = Number(process.env.PORT);
+const KEY_PATH = process.env.KEY_PATH;
+const CERT_PATH = process.env.CERT_PATH;
 
 const app = express();
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-  console.log('Server is running on port: %d', PORT);
-});
 
 app.use(cors({credentials: true}));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
+
+const privateKey = fs.readFileSync(KEY_PATH, 'utf8');
+const certificate = fs.readFileSync(CERT_PATH, 'utf8');
+
+// Create an HTTPS service with the certificate and private key.
+const server = https.createServer({ key: privateKey, cert: certificate }, app);
+
+server.listen(PORT, () => {
+  console.log('Server is running on port: %d', PORT);
+});
 
 const MONGODB_URL = process.env.MONGODB_URL
 
