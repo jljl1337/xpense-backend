c3c27c435306f5beebd40187cff6071db87f9779
Jack Leung <lckjack123@gmail.com>
2024-02-28 10:48:14 +0800

    Update HTTP method for updating user category to patch

--- src/router/users.ts
+++ src/router/users.ts
@@ -10,6 +10,6 @@ export default (router: express.Router): void => {
 
   router.post('/users/:userId/categories', isAuthenticated, addUserCategory);
   router.get('/users/:userId/categories', isAuthenticated, getUserCategories);
-  router.put('/users/:userId/categories/:categoryId', isAuthenticated, updateUserCategory);
+  router.patch('/users/:userId/categories/:categoryId', isAuthenticated, updateUserCategory);
   router.delete('/users/:userId/categories/:categoryId', isAuthenticated, deleteUserCategory);
 };
