17cdf4ac831a5e97cbc3ae78d79a625e83a239bd
Jack Leung <lckjack123@gmail.com>
2024-02-29 14:42:25 +0800

    Commented out addTokenToUser function

--- src/db/users.ts
+++ src/db/users.ts
@@ -29,7 +29,7 @@ export const UserModel = mongoose.model('User', UserSchema);
 export const getUsers = () => UserModel.find();
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 // export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
-export const addTokenToUser = (id: string, token: string, lastUsed: Date) => UserModel.findByIdAndUpdate(id, { $push: { 'authentication.tokens': { token, lastUsed } } });
+// export const addTokenToUser = (id: string, token: string, lastUsed: Date) => UserModel.findByIdAndUpdate(id, { $push: { 'authentication.tokens': { token, lastUsed } } });
 export const getUserById = (id: string) => UserModel.findById(id);
 export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
 // export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
