a3704da9b76f270e318abd86d51223dc3318c145
jljl1337 <lckjack123@gmail.com>
2025-09-09 20:07:58 -0600

    feat: enforce unique constraints on email and token fields in user and session tables

--- internal/sql/migration/1757403264041_user.sql
+++ internal/sql/migration/1757403264041_user.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user (
     id TEXT PRIMARY KEY,
-    email TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     created_at INTEGER NOT NULL,
     updated_at INTEGER NOT NULL
--- internal/sql/migration/1757420616352_session.sql
+++ internal/sql/migration/1757420616352_session.sql
@@ -1,7 +1,7 @@
 CREATE TABLE session (
     id TEXT PRIMARY KEY,
     user_id TEXT NOT NULL REFERENCES user(id) ON DELETE CASCADE,
-    token TEXT NOT NULL,
+    token TEXT NOT NULL UNIQUE,
     csrf_token TEXT NOT NULL,
     expires_at INTEGER NOT NULL,
     last_used_at INTEGER,
