9a699c6943b50529db43edcd6db0c42bbcb035f5
jljl1337 <lckjack123@gmail.com>
2025-09-10 00:35:09 -0600

    feat: refactor main function and implement server initialization with middleware

--- cmd/xpense-backend/main.go
+++ cmd/xpense-backend/main.go
@@ -1,28 +1,18 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
 	"log/slog"
-
-	"github.com/segmentio/ksuid"
 
 	"github.com/jljl1337/xpense-backend/internal/db"
 	"github.com/jljl1337/xpense-backend/internal/env"
 	"github.com/jljl1337/xpense-backend/internal/log"
-	"github.com/jljl1337/xpense-backend/internal/repository"
+	"github.com/jljl1337/xpense-backend/internal/server"
 )
 
 func main() {
-	env.LoadEnvFile()
+	env.LoadOptionalEnvFile()
 
 	log.SetCustomLogger()
-
-	slog.Debug(ksuid.New().String())
-	slog.Info(ksuid.New().String())
-	slog.Warn(ksuid.New().String())
-	slog.Error(ksuid.New().String())
 
 	// Migrate the database
 	dbInstance, err := db.NewDB()
@@ -33,35 +23,42 @@ func main() {
 
 	if err := db.Migrate(dbInstance); err != nil {
 		slog.Error("Failed to migrate database: " + err.Error())
-	}
-
-	queries := db.NewRepositoryQueries(dbInstance)
-	ctx := context.Background()
-	queries.CreateUser(ctx, repository.CreateUserParams{
-		ID:           ksuid.New().String(),
-		Email:        "user@example.com",
-		PasswordHash: "hashed_password",
-		CreatedAt:    1234567890,
-		UpdatedAt:    1234567890,
-	})
-	queries.CreateUser(ctx, repository.CreateUserParams{
-		ID:           ksuid.New().String(),
-		Email:        "user@example.com",
-		PasswordHash: "hashed_password",
-		CreatedAt:    1234567890,
-		UpdatedAt:    1234567890,
-	})
-	user, err := queries.GetUser(ctx, "some_user_id")
-	// user, err := queries.GetUser(ctx)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			slog.Info("No user found")
-			return
-		}
-		slog.Error("Failed to get user: " + err.Error())
 		return
 	}
 
-	slog.Info(fmt.Sprintf("User: %s", user.ID))
-	_ = user
+	// queries := db.NewRepositoryQueries(dbInstance)
+	// ctx := context.Background()
+	// queries.CreateUser(ctx, repository.CreateUserParams{
+	// 	ID:           ksuid.New().String(),
+	// 	Email:        "user@example.com",
+	// 	PasswordHash: "hashed_password",
+	// 	CreatedAt:    1234567890,
+	// 	UpdatedAt:    1234567890,
+	// })
+	// queries.CreateUser(ctx, repository.CreateUserParams{
+	// 	ID:           ksuid.New().String(),
+	// 	Email:        "user@example.com",
+	// 	PasswordHash: "hashed_password",
+	// 	CreatedAt:    1234567890,
+	// 	UpdatedAt:    1234567890,
+	// })
+	// user, err := queries.GetUser(ctx, "some_user_id")
+	// // user, err := queries.GetUser(ctx)
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		slog.Info("No user found")
+	// 		return
+	// 	}
+	// 	slog.Error("Failed to get user: " + err.Error())
+	// 	return
+	// }
+
+	// slog.Info(fmt.Sprintf("User: %s", user.ID))
+	// // slog.Info(fmt.Sprintf("User count: %d", len(user)))
+	// _ = user
+	server := server.NewServer(dbInstance)
+	if err := server.Start(); err != nil {
+		slog.Error("Failed to start server: " + err.Error())
+		return
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,46 @@
 package server
+
+import (
+	"database/sql"
+	"log/slog"
+	"net/http"
+
+	"github.com/jljl1337/xpense-backend/internal/repository"
+	"github.com/jljl1337/xpense-backend/internal/server/middleware"
+	"github.com/jljl1337/xpense-backend/internal/server/route"
+	"github.com/jljl1337/xpense-backend/internal/service"
+)
+
+type Server struct {
+	queries    *repository.Queries
+	httpServer *http.Server
+}
+
+func NewServer(db *sql.DB) *Server {
+	queries := repository.New(db)
+	mux := http.NewServeMux()
+	route.RegisterHealthRoutes(mux)
+
+	userService := service.NewUserService(queries)
+	authService := service.NewAuthService(userService)
+
+	middlewareProvider := middleware.NewMiddlewareProvider(authService)
+
+	stack := middleware.CreateStack(
+		middlewareProvider.CORS(),
+		middlewareProvider.Logging(),
+	)
+
+	return &Server{
+		queries: queries,
+		httpServer: &http.Server{
+			Addr:    ":8080",
+			Handler: stack(mux),
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	slog.Info("Starting server")
+	return s.httpServer.ListenAndServe()
+}
